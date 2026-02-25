7af3b4c17b57db95d72d6bf5a4f0e0245ccd45dd
Devarasetty-Prethikesh <dp9141@srmist.edu.in>
2026-02-25 11:02:42 +0530

    Pratikesh Added UC1-ApplicationEntry

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,14 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("   Welcome to PalindromeChecker  ");
+        System.out.println("=================================");
+        System.out.println("Application Name : PalindromeCheckerApp");
+        System.out.println("Version : 1.0");
+        System.out.println("Developed for Learning Purpose");
+        System.out.println("=================================");
+
+    }
+}
